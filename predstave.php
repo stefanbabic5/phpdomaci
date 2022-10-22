ef9d0461df36a12b35f3b0162c9f62260890d61b
Stefan Babic <stefanbabic37@gmail.com>
2022-10-22 23:43:31 +0200

    uradjen izgled stranica repertoar i predstave

--- predstave.php
+++ predstave.php
@@ -2,5 +2,42 @@
     include 'header.php';
 ?>
 
-    <p>Predstave</p>
+<div class='container mt-5'>
+    <h1 class="text-center">Spisak predstava</h1>
+    <div class='row mt-4'>
+        <div class='col-8'>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Naziv</th>
+                        <th>Trajanje</th>
+                        <th>Ocena</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody id='predstave'>
+
+                </tbody>
+            </table>
+        </div>
+        <div class='col-4'>
+            <h3>Unos predstave</h3>
+            <form id='forma'>
+                <label>Naziv</label>
+                <input class="form-control" type="text" id='naziv'>
+                <label>Trajanje</label>
+                <input class="form-control" type="number" min='0' id='trajanje'>
+                <label>Ocena</label>
+                <input class="form-control" type="number" min='0' max='10' id='ocena'>
+                <button id='sacuvaj' class="btn btn-primary form-control mt-2">Kreiraj</button>
+            </form>
+            <button id='obrisi' class="btn btn-danger mt-2 form-control" hidden>Obrisi</button>
+            <button id='nazad' class="btn btn-secondary mt-2 form-control" hidden>Nazad</button>
+        </div>
+    </div>
+</div>
+<script>
+
+</script>
 </body>
--- repertoar.php
+++ repertoar.php
@@ -2,5 +2,40 @@
     include 'header.php';
 ?>
 
-    <p>Repertoar</p>
+<div class="container mt-4">
+    <h1>Zakazani termini</h1>
+    <div class="row mt-3">
+        <div class="col-8">
+            <table class="mt-3 table table-dark">
+                <thead>
+                    <tr>
+                        <th>Naziv predstave</th>
+                        <th>Cena ulaznice</th>
+                        <th>Trajanje predstave</th>
+                        <th>Ocena/kritika</th>
+                        <th>Scena</th>
+                        <th>Pocetak</th>
+                    </tr>
+                </thead>
+                <tbody id='repertoar'>
+
+                </tbody>
+            </table>
+        </div>
+        <div class="col-4">
+            <h3 class="text-center">Kreiraj termin za predstavu</h3>
+            <form id='forma'>
+                <label>Cena karte</label>
+                <input class="form-control" type="number" min='0' id='cena'>
+                <label>Datum odrzavanja</label>
+                <input class="form-control" type="datetime-local" id='datum'>
+                <label>Predstava</label>
+                <select class="form-control" id='predstava'></select>
+                <label>Scena</label>
+                <select class="form-control" id='scena'></select>
+                <button class="btn btn-primary form-control mt-2">Kreiraj</button>
+            </form>
+        </div>
+    </div>
+</div>
 </body>
