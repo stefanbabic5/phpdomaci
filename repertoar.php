24832d0cefc0bfc792edf5ee9022e4d09df8a928
Stefan Babic <stefanbabic37@gmail.com>
2022-10-23 18:17:40 +0200

    zakazivanje termina predstave

--- repertoar.php
+++ repertoar.php
@@ -26,7 +26,7 @@
             <h3 class="text-center">Kreiraj termin za predstavu</h3>
             <form id='forma'>
                 <label>Cena karte</label>
-                <input class="form-control" type="number" min='0' id='cena'>
+                <input class="form-control" type="number" step="any" min='0' id='cena'>
                 <label>Datum odrzavanja</label>
                 <input class="form-control" type="datetime-local" id='datum'>
                 <label>Predstava</label>
@@ -38,4 +38,88 @@
         </div>
     </div>
 </div>
+<script>
+    let raspored = [];
+    $(document).ready(function () {
+        ucitajRaspored();
+        $("#forma").submit(function (e) {
+            e.preventDefault();
+            const cena=$("#cena").val();
+            const datum=$("#datum").val();
+            const predstava=$("#predstava").val();
+            const scena=$("#scena").val();
+            $.post("./server/index.php?akcija=raspored.create",{
+                predstavaID: predstava,
+                scenaID: scena,
+                datum,
+                cena
+            }, function(res) {
+                res=JSON.parse(res);
+                if(!res.status) {
+                    alert(res.error);
+                }
+                ucitajRaspored();
+            })
+        })
+        $.getJSON("./server/index.php?akcija=predstava.read",function (res){
+            if(!res.status) {
+                alert(res.error);
+                return;
+            }
+            for (let predstava of res.data) {
+                $("#predstava").append(`
+                <option value="${predstava.id}">${predstava.naziv}</option>
+                `)
+            }
+        })
+        $.getJSON("./server/index.php?akcija=scena.read",function (res){
+            if(!res.status) {
+                alert(res.error);
+                return;
+            }
+            for(let scena of res.data) {
+                $("#scena").append(`
+                <option value="${scena.id}">${scena.naziv}</option>
+                `)
+            }
+        })
+    })
+
+    function ucitajRaspored() {
+        $.getJSON("./server/index.php?akcija=raspored.read", function(res) {
+            if(!res.status) {
+                alert(res.error);
+                return;
+            }
+            raspored=res.data;
+            popuniTabelu();
+        });
+    }
+
+    function popuniTabelu() {
+        $("#repertoar").html('');
+        for (let stavkaRasporeda of raspored) {
+            $("#repertoar").append(`
+                <tr>
+                    <td>${stavkaRasporeda.predstava.naziv}</td>
+                    <td>${stavkaRasporeda.cena}</td>
+                    <td>${stavkaRasporeda.predstava.trajanje}</td>
+                    <td>${stavkaRasporeda.predstava.ocena}</td>
+                    <td>${stavkaRasporeda.scena.naziv}</td>
+                    <td>${stavkaRasporeda.datum}</td>
+                    <td><button class='btn btn-danger width-100' onClick="obrisi(${stavkaRasporeda.id})">Obrisi</button></td>
+                </tr>
+            `)
+        }
+    }
+    function obrisi(id) {
+        $.post("./server/index.php?akcija=raspored.delete",{id},function(res) {
+            res=JSON.parse(res);
+            if(!res.status){
+                alert(res.error);
+            }
+            ucitajRaspored();
+        })
+    }
+</script>
 </body>
